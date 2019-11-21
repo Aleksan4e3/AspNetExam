33bf9f1e3484207f88b4a7518eb61e1036e046c6
Aleksan4e3 <aleksan4e3@gmail.com>
2019-11-21 17:21:04 +0300

    Add new logging

--- Exam/Exam/Models/ExamContext.cs
+++ Exam/Exam/Models/ExamContext.cs
@@ -1,12 +1,14 @@
 namespace Exam.Models
 {
-    using System;
+    using log4net;
     using System.Data.Entity;
-    using System.ComponentModel.DataAnnotations.Schema;
     using System.Linq;
+    using System.Threading.Tasks;
 
     public partial class ExamContext : DbContext
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(ExamContext));
+
         public ExamContext()
             : base("name=ExamContext")
         {
@@ -24,6 +26,37 @@ namespace Exam.Models
                 .HasMany(e => e.SubCategories)
                 .WithOptional(e => e.Parent)
                 .HasForeignKey(e => e.ParentId);
+        }
+
+        public override Task<int> SaveChangesAsync()
+        {
+            var addedEntities = ChangeTracker.Entries<Category>()
+                .Where(c => c.State == EntityState.Added)
+                .Select(c => c.Entity);
+
+            var modifiedEntities = ChangeTracker.Entries<Category>()
+                .Where(c => c.State == EntityState.Modified)
+                .Select(c => c.Entity);
+
+            var deletedEntities = ChangeTracker.Entries<Category>()
+                .Where(c => c.State == EntityState.Deleted)
+                .Select(c => c.Entity);
+
+            foreach (var added in addedEntities)
+            {
+                Log.Info($"Added category {{Id = {added.Id}, Name = {added.Name}, ParentId = {added.ParentId}}}");
+            }
+
+            foreach (var modified in modifiedEntities)
+            {
+                Log.Info($"Modified category {{Id = {modified.Id}, Name = {modified.Name}, ParentId = {modified.ParentId}}}");
+            }
+
+            foreach (var deleted in deletedEntities)
+            {
+                Log.Info($"Deleted category {{Id = {deleted.Id}, Name = {deleted.Name}, ParentId = {deleted.ParentId}}}");
+            }
+            return base.SaveChangesAsync();
         }
     }
 }
